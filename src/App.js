1b958f89de82dbfffc2d837989f8d012bc09e15f
hColleen <hcolleen@gmail.com>
2018-10-09 17:24:15 -0700

    attempt at markers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import BurgerMenu from './component/Menu';
 import Map from './component/Map'
 import SquareAPI from './API/';
 
 
 class App extends Component {
 
-  
   constructor(){
     super();
     this.state = {
@@ -24,30 +22,27 @@ class App extends Component {
       const { venues } = results.response;
       const { markers } = venues.map(venue => {
         return{
-            lat: venue.location.lat,
+            lat: venue.location.lat, 
             lng: venue.location.lng,
-            title: venue.name,
             isOpen: false,
             isVisible: true
-            }
-        })
+        }
+      })
         this.setState({ venues, markers })
         console.log(results)
       }).catch(error => {
         alert('FourSquare API Failed. Please check connection and try again')
+        console.log(error);
       })
  }
 
   render() {
     return (
-        <main>
-          <BurgerMenu {...this.state} />
-          <Map />
-        </main>
+          <Map {...this.state} />
     );  
   }
 }
 
 export default App;
 
-//tutorial from here: https://www.youtube.com/watch?v=ywdxLNjhBYw&list=PLgOB68PvvmWCGNn8UMTpcfQEiITzxEEA1
+//tutorial from here: https://www.youtube.com/watch?v=ywdxLNjhBYw&list=PLgOB68PvvmWCGNn8UMTpcfQEiITzxEEA1 and from here: https://www.youtube.com/watch?v=Q0vzqlnWWZw&list=PL4rQq4MQP1crXuPtruu_eijgOUUXhcUCP&index=2
--- src/component/Map.js
+++ src/component/Map.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+const MyMapComponent = withScriptjs(
+    withGoogleMap((props) =>
   <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultZoom={14}
+    defaultCenter={{ lat: 33.42, lng: -111.83}}
   >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+    {props.markers &&
+        props.markers
+        .filter(marker => marker.isVisible)
+        .map((marker, idx) => (
+            <Marker key={idx} position = {{ lat: marker.lat, lng: marker.lng }} />
+        ))
+     }
   </GoogleMap>
 ))
-
 
 
 export default class Map extends Component{
     render() {
         return(
             <MyMapComponent
-            isMarkerShown
+                {...this.props}
                 googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCnPeVOPbLkPtwjEbH9MKDppTkoFSVmKdA"
                 loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `400px` }} />}
+                containerElement={<div style={{ height: `100vh` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
             />
         )
